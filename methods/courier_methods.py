063ca40979bd21c8c4f9d3a2d887ea6e84da3878
Denis S <dskvortsov79@yandex.ru>
2025-07-15 10:09:38 +0300

    fixed missing allure.step annotation

--- methods/courier_methods.py
+++ methods/courier_methods.py
@@ -1,9 +1,11 @@
 import requests
+import allure
 from endpoints import Urls
 
 class CourierMethods:
 
     @staticmethod
+    @allure.step('Логин Курьера')
     def courier_login(payload):
         response = requests.post(Urls.COUIRIER_LOGIN_URL, json=payload)
         status_code = response.status_code
@@ -11,6 +13,7 @@ class CourierMethods:
         return status_code, formated_response
     
     @staticmethod
+    @allure.step('Создание Курьера')
     def courier_create(payload):
         response = requests.post(Urls.COURIER_URL, json=payload)
         status_code = response.status_code
@@ -18,6 +21,7 @@ class CourierMethods:
         return status_code, formated_response
     
     @staticmethod
+    @allure.step('Удаление Курьера')
     def courier_delete(id):
         payload = {
             "id": id
--- methods/order_methods.py
+++ methods/order_methods.py
@@ -1,9 +1,11 @@
 import requests
+import allure
 from endpoints import Urls
 
 class OrderMethods:
 
     @staticmethod
+    @allure.step('Создание Заказа')
     def order_create(payload):
         response = requests.post(Urls.ORDER_URL, json=payload)
         status_code = response.status_code
@@ -11,6 +13,7 @@ class OrderMethods:
         return status_code, formated_response
     
     @staticmethod
+    @allure.step('Получение списка Заказов')
     def get_list_of_orders():
         response = requests.get(Urls.ORDER_URL)
         status_code = response.status_code
@@ -18,6 +21,7 @@ class OrderMethods:
         return status_code, len(list_of_orders["orders"])
     
     @staticmethod
+    @allure.step('Подтверждение Заказа')
     def order_accept(order_id, courier_id):
         endpoint = f'{Urls.ORDER_ACCEPT}{order_id}?courierId={courier_id}'
         response = requests.put(endpoint)
@@ -26,6 +30,7 @@ class OrderMethods:
         return status_code, formated_response
 
     @staticmethod
+    @allure.step('Получение информации о Заказе')
     def get_order_details(order_id):
         endpoint = f'{Urls.ORDER_TRACK_URL}?t={order_id}'
         response = requests.get(endpoint)
